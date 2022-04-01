c645e3a684486699c09bea26c737a3efc09ba1e5
Brandon Wang <brandonwang2000@gmail.com>
2022-03-31 22:51:55 -0400

    front ultrasonic sensors work well for alignment

--- ultrasonic_sensors/ultrasonic_sensors.ino
+++ ultrasonic_sensors/ultrasonic_sensors.ino
@@ -1,9 +1,8 @@
 // each ultrasonic sensor is defined by [TRIG, ECHO]
-//FRONT_1 = [11, 13];
-//FRONT_2 = [11, 12];
-//LEFT    = [8, 10];
-//RIGHT   = [8, 9];
-int ALL_US[4][2] = {{11,13}, {11,12}, {8,10}, {8,9}};
+// FRONT_1, FRONT_2, LEFT, RIGHT
+int ALL_US[4][2] = {{11,13}, {7,12}, {8,10}, {8,9}};
+int LED_5V  = 3;
+int LED_GND = 4;
 
 void setup() {
   Serial.begin(9600);
@@ -11,6 +10,10 @@ void setup() {
     pinMode(ALL_US[i][0], OUTPUT);
     pinMode(ALL_US[i][1], INPUT);
   }
+
+  pinMode(LED_5V, OUTPUT);
+  pinMode(LED_GND, OUTPUT);
+  digitalWrite(LED_GND, LOW);
 }
 
 double get_distance(int U[]) {
@@ -30,12 +33,21 @@ void loop() {
   // put your main code here, to run repeatedly:
   float front_1_d = get_distance(ALL_US[0]);
   float front_2_d = get_distance(ALL_US[1]);
-  float left_d    = get_distance(ALL_US[2]);
-  float right_d   = get_distance(ALL_US[3]);
+//  float left_d    = get_distance(ALL_US[2]);
+//  float right_d   = get_distance(ALL_US[3]);
+
   Serial.print("Front 1 (in.): "); Serial.println(front_1_d);
   Serial.print("Front 2 (in.): "); Serial.println(front_2_d);
-  Serial.print("Left (in.): ");    Serial.println(left_d);
-  Serial.print("Right (in.): ");   Serial.println(right_d);
+//  Serial.print("Left (in.): ");    Serial.println(left_d);
+//  Serial.print("Right (in.): ");   Serial.println(right_d);
+
+  float p_dif = abs(front_1_d - front_2_d) / 2 / (front_1_d + front_2_d) * 100;
+  Serial.print("Percent Difference: "); Serial.print(p_dif); Serial.println("%");
+  if( (p_dif < 5 && front_1_d < 5) || (p_dif < 3 && front_1_d < 10) || (p_dif < 1 && front_1_d < 50) )
+    analogWrite(LED_5V, 2 * 255.0/5);
+  else
+    digitalWrite(LED_5V, LOW);
   Serial.println();
+  delay(200);
   
 }
